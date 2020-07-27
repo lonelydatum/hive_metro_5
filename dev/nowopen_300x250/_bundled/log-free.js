c587aa9d8c1d9f2ae0e1c6d4e93650eaca847e25
Gar <gar@lonelydatu.com>
2020-07-27 14:22:54 -0400

    one

--- dev/nowopen_300x250/_bundled/log-free.js
+++ dev/nowopen_300x250/_bundled/log-free.js
@@ -10,24 +10,29 @@ var size = { w: banner.offsetWidth, h: banner.offsetHeight };
 TweenLite.defaultEase = Power3.easeInOut;
 
 function start() {
+	var type = arguments.length <= 0 || arguments[0] === undefined ? "COMING_SOON" : arguments[0];
+
+	var t2Time = type === "COMING_SOON" ? 1 : .6;
 	var tl = new TimelineMax();
+	tl.set(".frame1", { opacity: 1 });
 	tl.from(".t1", .3, { opacity: 0 });
 	tl.to(".t1", .3, { opacity: 0 }, "+=1.8");
 
 	tl.from(".t2", .3, { opacity: 0 });
-	tl.from(".t2a", .3, { opacity: 0 }, "+=1");
+	tl.from(".t2a", .3, { opacity: 0 }, "+=" + t2Time);
 	tl.to(".t2a", .3, { opacity: 0 }, "+=3");
 	tl.from(".t2b", .3, { opacity: 0 });
 }
 
-start();
-
 exports.size = size;
+exports.start = start;
 
 },{}],2:[function(require,module,exports){
 "use strict";
 
-require("../../_common/js/common.js");
+var _commonJsCommonJs = require("../../_common/js/common.js");
+
+(0, _commonJsCommonJs.start)("NOW_OPEN");
 
 module.exports = {};
 
